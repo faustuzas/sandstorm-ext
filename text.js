c85b951d4012af029ffb0acd6a57142f9a2478b9
Audun Torp <audun@zedge.net>
2020-12-16 16:27:27 +0100

    Add confetti

--- text.js
+++ text.js
@@ -1,13 +1,21 @@
 window.addEventListener('load', (event) => {
     document.body.innerHTML = document.body.innerHTML.replaceAll('Boros Guildgate', '<span class="zedge_tooltip">Boros Guildgate<span class="zedge_tooltiptext"><img src="https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=366390&type=card" alt="Boros Guildgate"></span></span>')
-    let re = /(Boros Guildgate)/;
-    let node = document.body.textContent.match(re);
-    console.log(node)
-    if (document.body.textContent.match(re)) {
-        console.log('Found Boros Guildgate')
-        let match = re.exec(node)
-        if (match && match.length) {
-            console.log('match', match[0])
+    document.body.innerHTML = document.body.innerHTML.replaceAll('some games', '<span class="zedge-confetti">some games</span>')
+    let classname = document.getElementsByClassName("zedge-confetti");
+
+    var animateButton = function(e) {
+        e.preventDefault;
+        if (!e.target.classList.contains('animate'))
+        {
+            e.target.classList.add('animate');
         }
+        setTimeout(function(){
+            e.target.classList.remove('animate');
+        },700);
+    };
+
+    console.log('confetties', classname.length)
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('mouseover', animateButton, false);
     }
 });
