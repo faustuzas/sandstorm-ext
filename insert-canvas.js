79f372eacb44b0f9af3ecf80d15cb489e0b9f8a9
Faustas Butkus <faustas.butkus@gmail.com>
2020-12-17 14:21:35 +0200

    stuff

---
+++ insert-canvas.js
@@ -0,0 +1,10 @@
+var canvas = document.createElement("canvas");
+canvas.classList.add('fireworks')
+canvas.style.position = 'absolute'
+canvas.style.left = 0
+canvas.style.right = 0
+canvas.style.pointerEvents = 'none'
+
+outerDiv.appendChild(canvas)
+
+document.body.appendChild(outerDiv)
